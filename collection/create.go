855f7c5e3cf2110f523a3ff95deff80c00089f12
johnhof <john.hofrichter@gmail.com>
2016-02-14 09:40:18 -0800

    readme update

---
+++ collection/create.go
@@ -0,0 +1,6 @@
+package collection
+
+// Create creates a new record in the database
+func (c *Definition) Create() error {
+	return nil
+}
---
+++ collection/read.go
@@ -0,0 +1,11 @@
+package collection
+
+// Read retrieves a collection from the database
+func (c *Definition) Read() error {
+	return nil
+}
+
+// ReadOne retrieves a set of collections from the database
+func (c *Definition) ReadOne(id string) error {
+	return nil
+}
